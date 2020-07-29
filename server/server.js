a6586810d7cfbcf0f55e86f04c19b3e41d57f6f5
Kumar <kkaushal@ongraph.com>
2020-07-29 12:25:42 +0530

    init

---
+++ server/server.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+var httpServer = require('http');
+const ioServer = require('socket.io');
+const RTCMultiConnectionServer = require('rtcmulticonnection-server');
+
+const jsonPath = {
+    config: 'config.json',
+    logs: 'logs.json'
+};
+
+const BASH_COLORS_HELPER = RTCMultiConnectionServer.BASH_COLORS_HELPER;
+const getValuesFromConfigJson = RTCMultiConnectionServer.getValuesFromConfigJson;
+const getBashParameters = RTCMultiConnectionServer.getBashParameters;
+// const resolveURL = RTCMultiConnectionServer.resolveURL;
+
+var config = getValuesFromConfigJson(jsonPath);
+config = getBashParameters(config, BASH_COLORS_HELPER);
+
+var PORT = config.port;
+var isUseHTTPs = config.isUseHTTPs;
+
+function serverHandler(request, response) {
+    response.write('Server running'); //write a response to the client
+    response.end(); //end the response
+}
+
+var httpApp;
+
+if (isUseHTTPs) {
+    httpServer = require('https');
+
+    // See how to use a valid certificate:
+    // https://github.com/muaz-khan/WebRTC-Experiment/issues/62
+    var options = {
+        key: null,
+        cert: null,
+        ca: null
+    };
+
+    var pfx = false;
+
+    if (!fs.existsSync(config.sslKey)) {
+        console.log(BASH_COLORS_HELPER.getRedFG(), 'sslKey:\t ' + config.sslKey + ' does not exist.');
+    } else {
+        pfx = config.sslKey.indexOf('.pfx') !== -1;
+        options.key = fs.readFileSync(config.sslKey);
+    }
+
+    if (!fs.existsSync(config.sslCert)) {
+        console.log(BASH_COLORS_HELPER.getRedFG(), 'sslCert:\t ' + config.sslCert + ' does not exist.');
+    } else {
+        options.cert = fs.readFileSync(config.sslCert);
+    }
+
+    if (config.sslCabundle) {
+        if (!fs.existsSync(config.sslCabundle)) {
+            console.log(BASH_COLORS_HELPER.getRedFG(), 'sslCabundle:\t ' + config.sslCabundle + ' does not exist.');
+        }
+
+        options.ca = fs.readFileSync(config.sslCabundle);
+    }
+
+    if (pfx === true) {
+        options = {
+            pfx: sslKey
+        };
+    }
+
+    httpApp = httpServer.createServer(options, serverHandler);
+} else {
+    httpApp = httpServer.createServer(serverHandler);
+}
+
+RTCMultiConnectionServer.beforeHttpListen(httpApp, config);
+httpApp = httpApp.listen(process.env.PORT || PORT, process.env.IP || "0.0.0.0", function() {
+    RTCMultiConnectionServer.afterHttpListen(httpApp, config);
+});
+
+// --------------------------
+// socket.io codes goes below
+
+ioServer(httpApp).on('connection', function(socket) {
+    RTCMultiConnectionServer.addSocket(socket, config); 
+});
